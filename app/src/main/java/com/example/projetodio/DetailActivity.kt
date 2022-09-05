1e45d9173d2a80002a8d654bd6afd808c09fab15
MucaGomes <your@example.com>
2022-09-05 18:57:39 -0300

    Adicionando a primeira e segunda tela , e fazendo o front-end do app

---
+++ app/src/main/java/com/example/projetodio/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.projetodio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projetodio.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
